8e2aae86700ae130b422738719e466cba424b4ae
K-Summer <91466399+K-Summer@users.noreply.github.com>
2023-04-02 00:30:36 +0800

    Add files via upload

---
+++ pio/J1.js
@@ -0,0 +1,33 @@
+const sounds = {
+    "q": document.getElementById("sound_q"),
+    "w": document.getElementById("sound_w"),
+    "e": document.getElementById("sound_e"),
+    "r": document.getElementById("sound_r"),
+    "t": document.getElementById("sound_t"),
+    "y": document.getElementById("sound_y"),
+    "u": document.getElementById("sound_u"),
+    "a": document.getElementById("sound_a"),
+    "s": document.getElementById("sound_s"),
+    "d": document.getElementById("sound_d"),
+    "f": document.getElementById("sound_f"),
+    "g": document.getElementById("sound_g"),
+    "h": document.getElementById("sound_h"),
+    "j": document.getElementById("sound_j"),
+    "z": document.getElementById("sound_z"),
+    "x": document.getElementById("sound_x"),
+    "c": document.getElementById("sound_c"),
+    "v": document.getElementById("sound_v"),
+    "b": document.getElementById("sound_b"),
+    "n": document.getElementById("sound_n"),
+    "m": document.getElementById("sound_m"),
+    // 添加其他按键和音效
+};
+
+document.addEventListener("keydown", function (event) {
+    const key = event.key.toLowerCase();
+
+    if (key in sounds) {
+        sounds[key].currentTime = 0; // 播放前重置音频时间
+        sounds[key].play();
+    }
+});
---
+++ pio/an.js
@@ -0,0 +1,143 @@
+document.addEventListener("keydown", function (event) {
+    var divId;
+    switch (event.key) {
+        case "q":
+            divId = "q";
+            break;
+        case "w":
+            divId = "w";
+            break;
+        case "e":
+            divId = "e";
+            break;
+        case "r":
+            divId = "r";
+            break;
+        case "t":
+            divId = "t";
+            break;
+        case "y":
+            divId = "y";
+            break;
+        case "u":
+            divId = "u";
+            break;
+        case "a":
+            divId = "a";
+            break;
+        case "s":
+            divId = "s";
+            break;
+        case "d":
+            divId = "d";
+            break;
+        case "f":
+            divId = "f";
+            break;
+        case "g":
+            divId = "g";
+            break;
+        case "h":
+            divId = "h";
+            break;
+        case "j":
+            divId = "j";
+            break;
+        case "z":
+            divId = "z";
+            break;
+        case "x":
+            divId = "x";
+            break;
+        case "c":
+            divId = "c";
+            break;
+        case "v":
+            divId = "v";
+            break;
+        case "b":
+            divId = "b";
+            break;
+        case "n":
+            divId = "n";
+            break;
+        case "m":
+            divId = "m";
+            break;
+        default:
+            return;
+    }
+    document.getElementById(divId).style.backgroundColor = "#7e3131";
+});
+
+document.addEventListener("keyup", function (event) {
+    var divId;
+    switch (event.key) {
+        case "q":
+            divId = "q";
+            break;
+        case "w":
+            divId = "w";
+            break;
+        case "e":
+            divId = "e";
+            break;
+        case "r":
+            divId = "r";
+            break;
+        case "t":
+            divId = "t";
+            break;
+        case "y":
+            divId = "y";
+            break;
+        case "u":
+            divId = "u";
+            break;
+        case "a":
+            divId = "a";
+            break;
+        case "s":
+            divId = "s";
+            break;
+        case "d":
+            divId = "d";
+            break;
+        case "f":
+            divId = "f";
+            break;
+        case "g":
+            divId = "g";
+            break;
+        case "h":
+            divId = "h";
+            break;
+        case "j":
+            divId = "j";
+            break;
+        case "z":
+            divId = "z";
+            break;
+        case "x":
+            divId = "x";
+            break;
+        case "c":
+            divId = "c";
+            break;
+        case "v":
+            divId = "v";
+            break;
+        case "b":
+            divId = "b";
+            break;
+        case "n":
+            divId = "n";
+            break;
+        case "m":
+            divId = "m";
+            break;
+        default:
+            return;
+    }
+    document.getElementById(divId).style.backgroundColor = "#ffff";
+});
